980fbeb161f7386f4cc050a49905d1cef31f4cb9
Oatto319 <Oattowimolchai@gmail.com>
2025-06-20 14:05:09 +0700

    fisrtcommit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # flutter_empty_app
+
 ใบงานที่2 : การสร้าง layout
+
+A new Flutter project.
+
---
+++ lib/my_home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Wimolchai'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
